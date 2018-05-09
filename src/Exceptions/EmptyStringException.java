f76203e9182ee99eaeab28f34e9aebe0694ebadf
TomatoJoe24 <thomas.spanier24@googlemail.com>
2018-05-09 15:59:05 +0200

    1. Commit

---
+++ src/Exceptions/EmptyStringException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+public class EmptyStringException extends Exception {
+
+	public EmptyStringException() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Peer.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+/**
+ * @author Thomas Spanier
+ *
+ */
+public class Peer {
+
+	//Variablen
+	private InetAddress ipAddress;
+	//private HashMap photos;
+	
+	
+	
+	
+	//Constructor
+	public Peer() throws UnknownHostException{
+		
+			ipAddress = java.net.Inet4Address.getLocalHost();
+
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Peer peer = new Peer();
+			System.out.println(peer.ipAddress.getHostAddress() );
+			
+			
+			
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
