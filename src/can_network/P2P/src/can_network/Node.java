100ec122c0612e167dde431b20626fc0e1b52924
Memphis0815 <35699585+Memphis0815@users.noreply.github.com>
2018-05-28 09:06:02 +0200

    Add files via upload
    REST-Dummy mit den benötigten Paketen hinzugefügt

---
+++ src/can_network/P2P/src/can_network/Node.java
@@ -0,0 +1,9 @@
+package can_network;
+
+public class Node {
+
+	public Node() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/can_network/P2P/src/can_network/Peer.java
@@ -0,0 +1,48 @@
+package can_network;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+/**
+ * @author Thomas Spanier
+ *
+ */
+public class Peer {
+
+	//Variablen
+	private InetAddress ipAddress;
+	//private HashMap photos;
+	
+	
+	
+	
+	//Constructor
+	public Peer() throws UnknownHostException{
+		
+			ipAddress = java.net.Inet4Address.getLocalHost();
+
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Peer peer = new Peer();
+			System.out.println("IP-Addresse: " + peer.ipAddress.getHostAddress() );
+			
+			
+			InetAddress[] iad = InetAddress.getAllByName(InetAddress.getLocalHost().getHostName());
+			for(InetAddress i : iad) {
+				System.out.println(i);
+			}
+
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
